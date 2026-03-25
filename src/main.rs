257528dd1a788abfa67d168563ccc6deab177baf
Peter Bodrogi <p.bodrogi@erco.com>
2026-03-25 10:37:03 +0000

    nyul12

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() {
     // With explicit type annotation
     let pi: f64 = 3.1415926535897932384626;
     println!("pi = {}", pi);
-}
-let mut led_count: f64 = 6;
-let mut voltage: f64 = 34;
+    let mut led_count: f64 = 6.000;
+let mut voltage: f64 = 34.0000;
 let mut is_on: bool = false;
 println!("led_count = {}, voltage = {}, is_on = {}", led_count, voltage, is_on);
+}
